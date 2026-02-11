651d6765d0524c5ab16531fa3082d088b687fb3f
andreycpu <andreymarey20@gmail.com>
2026-02-11 04:43:03 +0000

    Add authentication methods: Bearer, Basic, API key, custom headers

--- agent_toolbox/api_client.py
+++ agent_toolbox/api_client.py
@@ -61,3 +61,28 @@ class APIClient:
         """Make a DELETE request."""
         response = self._make_request('DELETE', endpoint)
         return response.json() if response.content else {}
+        
+    def set_auth_bearer(self, token: str) -> None:
+        """Set Bearer token authentication."""
+        self.session.headers.update({'Authorization': f'Bearer {token}'})
+        
+    def set_auth_basic(self, username: str, password: str) -> None:
+        """Set Basic authentication."""
+        from requests.auth import HTTPBasicAuth
+        self.session.auth = HTTPBasicAuth(username, password)
+        
+    def set_auth_header(self, header_name: str, header_value: str) -> None:
+        """Set custom authentication header."""
+        self.session.headers.update({header_name: header_value})
+        
+    def set_api_key(self, key: str, param_name: str = 'api_key', in_header: bool = False) -> None:
+        """Set API key authentication."""
+        if in_header:
+            self.session.headers.update({param_name: key})
+        else:
+            # Store for query params - will be added in requests
+            self._api_key_param = {param_name: key}
+            
+    def add_default_params(self, params: Dict[str, str]) -> None:
+        """Add default parameters to all requests."""
+        self.session.params = params
